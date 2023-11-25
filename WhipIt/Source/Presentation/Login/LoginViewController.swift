be9f7478481411429d4a1e3a81b6acdf531c4004
Chaewon Yoon <ycw0363@gmail.com>
2023-11-25 22:37:19 +0900

    feat: LoginView UI 수정

--- WhipIt/Source/Presentation/Login/LoginViewController.swift
+++ WhipIt/Source/Presentation/Login/LoginViewController.swift
@@ -18,17 +18,9 @@ class LoginViewController: BaseViewController {
         return view
     }()
     
-    private lazy var emailLabel = UILabel.labelBuilder(text: "이메일 주소", font: Font.bold14, textColor: .black)
-    private lazy var emailTextField = UITextField.underlineTextFieldBuilder(placeholder: "예) whipit@whipit.co.kr", textColor: .black, textAlignment: .natural)
-    private lazy var passwordLabel = UILabel.labelBuilder(text: "비밀번호", font: Font.bold14, textColor: .black)
-    private lazy var passwordTextField = UITextField.underlineTextFieldBuilder(placeholder: "비밀번호를 입력해주세요", textColor: .black, textAlignment: .natural)
-    private lazy var loginButton = {
-        let view = UIButton.buttonBuilder(title: "로그인", font: Font.bold16)
-        view.backgroundColor = .black
-        view.setTitleColor(.white, for: .normal)
-        view.layer.cornerRadius = 10
-        return view
-    }()
+    private lazy var emailView: JoinView = JoinView(type: .email)
+    private lazy var passwordView: JoinView = JoinView(type: .password)
+    private lazy var loginButton: UIButton = BlackBackgroundRoundedButton(title: "로그인")
     private lazy var segmentedControl = {
         let view = UISegmentedControl(items: ["이메일 가입", "이메일 찾기", "비밀번호 찾기"])
         let font = Font.light14
@@ -71,17 +63,15 @@ class LoginViewController: BaseViewController {
         super.setHierarchy()
         
         view.addSubview(logoImageView)
-        view.addSubview(emailLabel)
-        view.addSubview(emailTextField)
-        view.addSubview(passwordLabel)
-        view.addSubview(passwordTextField)
+        view.addSubview(emailView)
+        view.addSubview(passwordView)
         view.addSubview(loginButton)
         view.addSubview(segmentedControl)
     }
     
     override func setConstraints() {
         super.setConstraints()
-//        logoImageView.backgroundColor = .blue
+        
         logoImageView.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
             make.centerX.equalToSuperview().multipliedBy(0.95)
@@ -89,28 +79,22 @@ class LoginViewController: BaseViewController {
             make.width.equalTo(200)
         }
         
-        emailLabel.snp.makeConstraints { make in
+        emailView.titleLabel.text = "이메일 주소"
+        emailView.snp.makeConstraints { make in
             make.top.equalTo(logoImageView.snp.bottom).offset(60)
-            make.leading.equalToSuperview().offset(20)
+            make.horizontalEdges.equalToSuperview()
+            make.height.equalTo(65)
         }
         
-        emailTextField.snp.makeConstraints { make in
-            make.top.equalTo(emailLabel.snp.bottom).offset(12)
-            make.horizontalEdges.equalToSuperview().inset(20)
+        passwordView.titleLabel.text = "비밀번호"
+        passwordView.snp.makeConstraints { make in
+            make.top.equalTo(emailView.snp.bottom).offset(20)
+            make.horizontalEdges.equalToSuperview()
+            make.height.equalTo(65)
         }
-        
-        passwordLabel.snp.makeConstraints { make in
-            make.top.equalTo(emailTextField.snp.bottom).offset(32)
-            make.leading.equalToSuperview().offset(20)
-        }
-        
-        passwordTextField.snp.makeConstraints { make in
-            make.top.equalTo(passwordLabel.snp.bottom).offset(12)
-            make.horizontalEdges.equalToSuperview().inset(20)
-        }
-        
+
         loginButton.snp.makeConstraints { make in
-            make.top.equalTo(passwordTextField.snp.bottom).offset(50)
+            make.top.equalTo(passwordView.snp.bottom).offset(40)
             make.horizontalEdges.equalToSuperview().inset(20)
             make.height.equalTo(50)
         }
