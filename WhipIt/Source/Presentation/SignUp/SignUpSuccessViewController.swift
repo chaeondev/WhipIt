f667dd7425caeb846c71e11832e46ebc9a9f2129
Chaewon Yoon <ycw0363@gmail.com>
2023-11-25 17:08:00 +0900

    Create SignUpSuccessViewController.swift

---
+++ WhipIt/Source/Presentation/SignUp/SignUpSuccessViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SignUpSuccessViewController.swift
+//  WhipIt
+//
+//  Created by Chaewon on 11/25/23.
+//
+
+import Foundation
+
+class SignUpSuccessViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func setHierarchy() {
+        super.setHierarchy()
+    }
+    
+    override func setConstraints() {
+        super.setConstraints()
+    }
+}
