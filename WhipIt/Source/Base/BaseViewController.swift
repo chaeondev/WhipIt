da0c7320ba3984f464755fd0011a1f79fd079419
Chaewon Yoon <ycw0363@gmail.com>
2023-11-25 16:46:59 +0900

    feat: AlertMessage BaseVC에 추가

--- WhipIt/Source/Base/BaseViewController.swift
+++ WhipIt/Source/Base/BaseViewController.swift
@@ -24,4 +24,19 @@ class BaseViewController: UIViewController {
     func setConstraints() {
         view.backgroundColor = .white
     }
+    
+    func showAlertMessage(title: String, message: String? = nil, handler: (() -> ())? = nil) {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+//        let cancel = UIAlertAction(title: "취소", style: .cancel)
+        let ok = UIAlertAction(title: "확인", style: .default) { _ in
+            handler?()
+        }
+        
+//        alert.addAction(cancel)
+        alert.addAction(ok)
+        
+        present(alert, animated: true)
+        
+    }
 }
