0ca34a6833811569fa0f1865c7b9bd2c9161e3ee
Chaewon Yoon <ycw0363@gmail.com>
2023-11-27 15:02:48 +0900

    feat: BaseCollectionViewCell 생성

---
+++ WhipIt/Source/Base/BaseCollectionViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  BaseCollectionViewCell.swift
+//  WhipIt
+//
+//  Created by Chaewon on 11/26/23.
+//
+
+import UIKit
+import SnapKit
+
+class BaseCollectionViewCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setHierarchy()
+        setConstraints()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setHierarchy() { }
+    func setConstraints() { }
+}
