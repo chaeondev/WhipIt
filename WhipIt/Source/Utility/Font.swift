fdd9ef83f077f0292e7a60b1ce091236bc77bced
Chaewon Yoon <ycw0363@gmail.com>
2023-11-30 03:55:35 +0900

    feat: Update Font

--- WhipIt/Source/Utility/Font.swift
+++ WhipIt/Source/Utility/Font.swift
@@ -8,11 +8,22 @@
 import UIKit
 
 enum Font {
+    static let light10 = UIFont(name: "SUITVariable-Light", size: 10)!
     static let light12 = UIFont(name: "SUITVariable-Light", size: 12)!
     static let light14 = UIFont(name: "SUITVariable-Light", size: 14)!
     static let bold14 = UIFont(name: "SUITVariable-Bold", size: 14)!
     static let bold16 = UIFont(name: "SUITVariable-Bold", size: 16)!
+    static let bold24 = UIFont(name: "SUITVariable-Bold", size: 24)!
     static let extraBold34 = UIFont(name: "SUITVariable-ExtraBold", size: 34)!
+}
+
+enum Suit {
+    static let light = "SUITVariable-Light"
+    static let medium = "SUITVariable-Medium"
+    static let semiBold = "SUITVariable-SemiBold"
+    static let bold = "SUITVariable-Bold"
+    static let extraBold = "SUITVariable-ExtraBold"
+    static let heavy = "SUITVariable-Heavy"
 }
 
 /*
