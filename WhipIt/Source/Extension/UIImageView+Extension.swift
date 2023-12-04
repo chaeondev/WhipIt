b1f54a7fd7b7973c689484b5a25f59c51ce0e726
Chaewon Yoon <ycw0363@gmail.com>
2023-12-05 05:39:28 +0900

    feat: KingFisher 이미지뷰 extension

---
+++ WhipIt/Source/Extension/UIImageView+Extension.swift
@@ -0,0 +1,26 @@
+//
+//  UIImageView+Extension.swift
+//  WhipIt
+//
+//  Created by Chaewon on 12/1/23.
+//
+
+import UIKit
+import Kingfisher
+
+extension UIImageView {
+    
+    func setKFImage(imageUrl: String, errorImage: UIImage? = nil) {
+        let modifier = AnyModifier { request in
+            var requestBody = request
+            requestBody.setValue(KeyChainManager.shared.accessToken, forHTTPHeaderField: "Authorization")
+            requestBody.setValue(APIKey.sesacKey, forHTTPHeaderField: "SesacKey")
+            return requestBody
+        }
+        
+        let url = URL(string: APIKey.baseURL + imageUrl)
+        
+        self.kf.indicatorType = .activity
+        self.kf.setImage(with: url, options: [.requestModifier(modifier)])
+    }
+}
