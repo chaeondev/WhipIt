59a5de8e1afb7d86b2b78f9fb0addc0ad87aa646
Chaewon Yoon <ycw0363@gmail.com>
2023-12-07 11:38:38 +0900

    feat: ViewBuilder 생성

---
+++ WhipIt/Source/Builder/ViewBuilder.swift
@@ -0,0 +1,16 @@
+//
+//  ViewBuilder.swift
+//  WhipIt
+//
+//  Created by Chaewon on 12/6/23.
+//
+
+import UIKit
+
+extension UIView {
+    static func barViewBuilder(color: UIColor) -> UIView {
+        let view = UIView()
+        view.backgroundColor = color
+        return view
+    }
+}
