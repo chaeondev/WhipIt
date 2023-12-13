15b3e30d267f55529bda6000fe28d63556114b39
Chaewon Yoon <ycw0363@gmail.com>
2023-12-13 15:56:42 +0900

    Update ButtonBuilder.swift

--- WhipIt/Source/Builder/ButtonBuilder.swift
+++ WhipIt/Source/Builder/ButtonBuilder.swift
@@ -8,11 +8,12 @@
 import UIKit
 
 extension UIButton {
-    static func buttonBuilder(image: UIImage? = nil, title: String? = nil, font: UIFont? = nil) -> UIButton {
+    static func buttonBuilder(image: UIImage? = nil, title: String? = nil, font: UIFont? = nil, titleColor: UIColor? = nil) -> UIButton {
         let view = UIButton()
         view.titleLabel?.font = font
         view.setTitle(title, for: .normal)
         view.setImage(image, for: .normal)
+        view.setTitleColor(titleColor, for: .normal)
         return view
     }
 }
