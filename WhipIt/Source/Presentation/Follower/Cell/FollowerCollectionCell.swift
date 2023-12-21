200e12698b5a70ef78a5e0228430162d3b02dcc9
Chaewon Yoon <ycw0363@gmail.com>
2023-12-22 01:30:03 +0900

    feat: 팔로우탭 collectionview 마지막 셀에 더보기 버튼 추가

--- WhipIt/Source/Presentation/Follower/Cell/FollowerCollectionCell.swift
+++ WhipIt/Source/Presentation/Follower/Cell/FollowerCollectionCell.swift
@@ -17,12 +17,28 @@ final class FollowerCollectionCell: BaseCollectionViewCell {
         return view
     }()
     
+    let profileButton = {
+        let view = UIButton.buttonBuilder(title: "더보기", font: UIFont(name: Suit.semiBold, size: 12)!, titleColor: .white)
+        view.backgroundColor = .black.withAlphaComponent(0.4)
+        return view
+    }()
+    
     override func setHierarchy() {
         contentView.addSubview(imageView)
+        contentView.addSubview(profileButton)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        profileButton.isHidden = true
     }
     
     override func setConstraints() {
         imageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        profileButton.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
     }
